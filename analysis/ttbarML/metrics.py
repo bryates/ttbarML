1523e4810c7cd1f00fced58b7edce269cef606f2
Brent Yates <yates.313@osu.edu>
2025-06-10 14:26:31 -0400

    Add msising files

---
+++ analysis/ttbarML/metrics.py
@@ -0,0 +1,45 @@
+import torch
+
+def net_eval(out, test_feat, test_input, threshold=0.5, n_points=200):
+    
+    #sm_hist, bsm_hist = make_hist(out, sm, bsm, n_points)
+    
+    roc, auc = make_roc(out, test_feat, n_points)
+    
+    #a   = (sm[(out <= threshold).flatten()].sum() + bsm[(out >= threshold).flatten()].sum())/(sm.sum() + bsm.sum())
+    
+    
+    return roc, auc#, a
+
+def make_hist(out, sm, bsm, n_points):
+    
+    sort_ind = torch.argsort(out, axis=0)
+    
+    bsm       = bsm[sort_ind]
+    sm        = sm[sort_ind]
+    bsm_total = bsm.sum()
+    sm_total  = sm.sum()
+    
+    disc = len(bsm)%n_points
+    
+    if disc == 0:
+        bsm = torch.sum(torch.tensor_split(bsm, n_points), axis=1)
+        sm  = torch.sum(torch.tensor_split(sm,  n_points), axis=1)
+        
+    else:
+        bsm = torch.cat((torch.sum(torch.stack(torch.tensor_split(bsm, n_points)[:disc]), axis=1),
+                         torch.sum(torch.stack(torch.tensor_split(bsm, n_points)[disc:]), axis=1)), axis=0)
+        sm  = torch.cat((torch.sum(torch.stack(torch.tensor_split(sm,  n_points)[:disc]), axis=1),
+                         torch.sum(torch.stack(torch.tensor_split(sm,  n_points)[disc:]), axis=1)), axis=0)
+
+    return sm, bsm
+
+def make_roc(out, test_feat, n_points):
+    
+    roc = torch.cat((torch.cumsum(out, dim=0),#.reshape(n_points,1),
+                     torch.cumsum(test_feat, dim=0)), axis=1)#.reshape(n_points,1)), axis=1)
+    roc = 1 - roc/roc[-1]
+
+    auc = -torch.trapz(roc[:,1], x=roc[:,0])
+    
+    return roc, auc   
